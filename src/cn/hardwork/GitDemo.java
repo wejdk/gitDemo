a7c6caf9d667f5994d3fa29b29acf580a1e7fc3b
wejdk <333>
2019-08-10 21:17:23 +0800

    把本地工程添加到git

---
+++ src/cn/hardwork/GitDemo.java
@@ -0,0 +1,16 @@
+package cn.hardwork;
+
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+        int i=0;
+    }
+}
